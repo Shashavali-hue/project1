f842df2167115be60aaf78866f0142a05b179d9e
Shashavali-hue <dudekulas136@gmail.com>
2025-11-06 10:56:42 +0530

    first commit

--- BackEnd/akbar.py
+++ BackEnd/akbar.py
@@ -1,77 +1,52 @@
-from flask import Flask, request, redirect, url_for, render_template_string
+from flask import Flask, request, redirect, render_template_string
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Configure database
+# Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///names.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Define the model
+# Model
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
-# Create the database
 with app.app_context():
     db.create_all()
 
-# Home route - asks for name
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        if name:
-            new_person = Person(name=name)
-            db.session.add(new_person)
-            db.session.commit()
-            return redirect(url_for('show_names'))
+# Route to take name via URL parameter
+@app.route('/')
+def home():
+    name = request.args.get('name')
+    if name:
+        new_person = Person(name=name)
+        db.session.add(new_person)
+        db.session.commit()
+        return redirect('/names')
 
     return render_template_string('''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Enter Name</title>
-        </head>
-        <body>
-            <h2>Enter a Name</h2>
-            <form method="POST">
-                <input type="text" name="name" placeholder="Enter your name" required>
-                <button type="submit">Submit</button>
-            </form>
-        </body>
-        </html>
+        <h2>Enter a name</h2>
+        <form action="/" method="get">
+            <input type="text" name="name" placeholder="Enter your name" required>
+            <input type="submit" value="Submit">
+        </form>
     ''')
 
-# Show all names
+# Route to display stored names
 @app.route('/names')
-def show_names():
+def names():
     people = Person.query.all()
     return render_template_string('''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Stored Names</title>
-        </head>
-        <body>
-            <h2>List of Stored Names</h2>
-            <table border="1">
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-                {% for person in people %}
-                <tr>
-                    <td>{{ person.id }}</td>
-                    <td>{{ person.name }}</td>
-                </tr>
-                {% endfor %}
-            </table>
-            <br>
-            <a href="{{ url_for('index') }}">Add Another Name</a>
-        </body>
-        </html>
+        <h2>Stored Names</h2>
+        <table border="1">
+            <tr><th>ID</th><th>Name</th></tr>
+            {% for p in people %}
+            <tr><td>{{ p.id }}</td><td>{{ p.name }}</td></tr>
+            {% endfor %}
+        </table>
+        <br><a href="/">Add another name</a>
     ''', people=people)
 
 if __name__ == '__main__':
